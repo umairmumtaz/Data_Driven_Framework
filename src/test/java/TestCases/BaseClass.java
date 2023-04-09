c86ae0ec2a87ca380a81d81f9a8ca92e434b7ec3
CREDENTIAL\umumtaz <umumtaz@aviso.ca>
2023-04-09 01:34:46 -0400

    updated chromeDriver

--- src/test/java/TestCases/BaseClass.java
+++ src/test/java/TestCases/BaseClass.java
@@ -1,7 +1,18 @@
 package TestCases;
 
-public class BaseClass {
-    // Page factpry
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class BaseClass {
+
+    // this is a duplicate method, as to just quickly check whether browser is getting open or not.
+    @Test
+     public void test1() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\umumtaz\\AquaProjects\\Data_Driven_Framework\\src\\test\\Resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String baseUrl = "https://www.wikipedia.org/";
+        driver.get(baseUrl);
+    }
 
 }
